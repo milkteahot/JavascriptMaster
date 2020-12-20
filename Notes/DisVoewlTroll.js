21cbff9d9ef6b83f236914c9a88fa035bb327735
milkteahot <greenegoode11@gmail.com>
2020-12-21 08:44:47 +0900

    DisemVowel Troll

---
+++ Notes/DisVoewlTroll.js
@@ -0,0 +1,11 @@
+const vowel = ['a','e','i','o','u','A','E','I','O','U'];
+
+function disemvowel(str) {
+    let array = [...str];
+    array = array.filter(v=>!vowel.includes(v))
+    return array.join("");
+  }
+
+  console.log(disemvowel("This website is for losers LOL!"));
+
+//review
