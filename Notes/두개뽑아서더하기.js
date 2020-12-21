a677c08ba71a11acb4a4a0b897c2629f90fccccb
milkteahot <greenegoode11@gmail.com>
2020-12-21 12:08:00 +0900

    Add review

--- Notes/두개뽑아서더하기.js
+++ Notes/두개뽑아서더하기.js
@@ -1,22 +1,26 @@
 const numbers = [5, 0, 2, 7];
 
-// function solution(numbers) {
-//     const answer = [];
-//     for(let i=0;i<numbers.length;i++){
-//         for(let j=0;j<numbers.length;j++){
-//             if(i !== j) answer.push(numbers[i]+numbers[j]);
-//         }
-//     }
-//     return answer.reduce((acc,cur)=>acc.includes(cur) ? acc:[...acc,cur],[]).sort((a,b)=>a-b);
-// }
+function solution(numbers) {
+    const answer = [];
+    for(let i=0;i<numbers.length;i++){
+        for(let j=0;j<numbers.length;j++){
+            if(i !== j) answer.push(numbers[i]+numbers[j]);
+        }
+    }
+    //중복제거를 위해 reduce() 사용
+    return answer.reduce((acc,cur)=>acc.includes(cur) ? acc:[...acc,cur],[]).sort((a,b)=>a-b);
+}
+
+//review
 function solution(numbers) {
   const temp = [];
   for (let i = 0; i < numbers.length; i++) {
+      //i+1 로 다음값부터 더하기
     for (let j = i + 1; j < numbers.length; j++) {
       temp.push(numbers[i] + numbers[j]);
     }
   }
-  console.log(temp, 'temp')
+  //중복제거를 위해 Set() 사용
   const answer = [...new Set(temp)];
   return answer.sort((a, b) => a - b);
 }
